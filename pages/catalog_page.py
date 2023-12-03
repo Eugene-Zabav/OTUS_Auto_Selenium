d3d7f60dece4469b165da5d43c14a97335affb6d
Eugene Zabavnyuk <mtc8584777@gmail.com>
2023-12-04 00:24:34 +0300

    Add logging

--- pages/catalog_page.py
+++ pages/catalog_page.py
@@ -8,11 +8,14 @@ class CatalogPage(BasePage):
     ITEM_PRICE = (By.XPATH, "//p[@class='price']")
 
     def open(self, url):
+        self.logger.info(f"open '{url + self.PATH}' url")
         self.browser.get(url + self.PATH)
 
     def change_currency(self):
+        self.logger.info("Change currency")
         HeaderElements(self.browser).change_currency_to_eur()
         return self
 
     def currency_price(self):
+        self.logger.info("Get price with current currency")
         return self.element(self.ITEM_PRICE)
